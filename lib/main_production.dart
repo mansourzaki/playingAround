e4161a2c090c663c2a5db6ff9c40b60053a80edb
Mansour <mansourzaki2000@gmail.com>
2022-10-11 11:57:15 +0300

    firebase integration

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,16 @@
 import 'package:bond/routes/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:one_studio_core/core.dart';
-
 import 'bond_app.dart';
+import 'firebase_options.dart';
 import 'injection_container.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await EasyLocalization.ensureInitialized();
   await init();
